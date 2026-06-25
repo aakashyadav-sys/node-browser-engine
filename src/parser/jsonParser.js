914c77a068801a1da18855bed0934d778a9c7977
aakasyadav-sys <aakasyadav@sysconinfotech.com>
2026-06-25 15:29:44 +0530

    added parse Node method for children

--- src/parser/jsonParser.js
+++ src/parser/jsonParser.js
@@ -28,6 +28,53 @@ class JosnParser {
 
         return rootNode;
     }
+
+    //Recursively  parse a json node
+
+    parseNode(data, parent, depth) {
+        if (!data || typeof data !== 'object') {
+            throw new Error(`Invalid  ndoe at depth ${depth}: ${JSON.stringify(dat)}`)
+        }
+
+        const type = this.determineNodeType(data);
+
+        // create the node 
+        const node = new Node(type, {
+            id: data?.id || null,
+            tagName: data?.tagName || data?.type || type,
+            value: data?.value || data?.text || null,
+            attributes: data?.attributes || {},
+            style: this.normalizeStyle(data?.style || {})
+        })
+
+        node.depth = depth;
+        node.parent = parent;
+        this.nodeCount++;
+
+        //parse children
+        if (data?.children && Array.isArray(data?.children)) {
+            for (const childData of data?.children) {
+                if (typeof childData === 'string') {
+                    const textNode = new Node('text', {
+                        value: children,
+                        style: { fontSize: node.style?.fontSize, color: node.style?.color }
+                    })
+                    textNode.depth = depth + 1;
+                    textNode.parent = node;
+                    this.nodeCount++;
+                    node.children.push(textNode);
+                } else {
+                    const childNode = this.parseNode(childData, node, depth + 1);
+                    node.children.push(childNode);
+                }
+            }
+        }
+
+        return node;
+
+    }
+
+    
 }
 
 
