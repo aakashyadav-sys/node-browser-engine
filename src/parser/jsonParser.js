523f4ebfeda8a39bb8cf45e5d68f2514b5a7bbea
aakasyadav-sys <aakasyadav@sysconinfotech.com>
2026-06-25 17:41:27 +0530

    added determineNodeType and normalize Styles methods for JsonParser class

--- src/parser/jsonParser.js
+++ src/parser/jsonParser.js
@@ -74,9 +74,96 @@ class JosnParser {
 
     }
 
-    
+
+    determineNodeType(data) {
+        if (data?.type) {
+            return data?.type;
+        }
+
+        if ((data?.value || data?.text) && !data?.children) {
+            return text;
+        }
+
+        if (data?.children && data?.children?.length > 0) {
+            return 'container';
+        }
+
+        return container;
+    }
+
+
+    normalizeStyles(style) {
+        const normalized = { ...style };
+
+        // normalize padding
+        if (typeof style?.padding === "number") {
+            normalized?.padding = {
+                top: style?.padding,
+                right: style?.right,
+                bottom: style?.bottom,
+                left: style?.left,
+            }
+        } else if (Array.isArray(style?.padding)) {
+            const [top, right = top, bottom = top, left = right] = style?.padding;
+            normalized?.padding = { top, right, bottom, left };
+        } else if (typeof style?.padding === 'object') {
+            normalized?.padding = {
+                top: style?.padding?.top || 0,
+                right: style?.padding?.right || 0,
+                bottom: style?.padding?.bottom || 0,
+                left: style?.padding?.left || 0,
+            }
+        } else {
+            normalized?.padding = { top: 0, right: 0, bottom: 0, left: 0 }
+        }
+        // normalize margin
+        if (typeof style?.margin === 'number') {
+            normalized?.margin = {
+                top: style?.margin,
+                right: style?.right,
+                bottom: style?.bottom,
+                left: style?.left,
+            }
+        } else if (Array.isArray(style?.margin)) {
+            const [top, right = top, bottom = top, left = right] = style?.margin;
+            normalized?.margin = { top, right, bottom, left }
+        } else if (typeof style?.margin === 'object') {
+            normalized?.padding = {
+                top: style?.margin?.top || 0,
+                right: style?.margin?.right || 0,
+                bottom: style?.margin?.bottom || 0,
+                left: style?.margin?.left || 0,
+            }
+        } else {
+            normalized?.margin = { top: 0, right: 0, bottom: 0, left: 0 }
+        }
+
+        // border 
+        if (typeof style?.border === "string") {
+            normalized?.border = {
+                width: 1,
+                color: style?.border,
+                style: "solid",
+            }
+        } else if (typeof style?.border === "object") {
+            normalized?.border = {
+                width: style?.border?.width || 0,
+                color: style?.border?.color || 0,
+                style: style?.border?.style || "solid",
+
+            }
+        } else if (typeof style?.border === "number") {
+            normalized?.border = {
+                width: style?.border,
+                color: "#000000",
+                style: "solid"
+            }
+        }
+
+        return normalized;
+    }
 }
 
 
 
-
+export default JosnParser;
